efdd7567079b923a5ac6a02cb6bd59013d826557
abhinesh chandra <abhineshchandra1234@gmail.com>
2021-05-05 21:22:57 +0530

    timer added but it is not stopping

--- app/src/main/java/com/example/matrixgeneration/NumberListAdapter.kt
+++ app/src/main/java/com/example/matrixgeneration/NumberListAdapter.kt
@@ -1,16 +1,23 @@
 package com.example.matrixgeneration
 
-import android.graphics.Color
+import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.matrixgeneration.databinding.RecyclerItemBinding
+
 
 class NumberListAdapter: RecyclerView.Adapter<NumberListAdapter.MyViewHolder>()  {
 
     var numberList = emptyList<Int>()
 
-    inner class MyViewHolder(val binding: RecyclerItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    inner class MyViewHolder(val binding: RecyclerItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        var timer: CountDownTimer? = null
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(RecyclerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
@@ -25,6 +32,26 @@ class NumberListAdapter: RecyclerView.Adapter<NumberListAdapter.MyViewHolder>() 
         }
 
         holder.binding.textViewNumber.text = "0"
+        holder.binding.textViewNumber.setOnClickListener {
+            if (holder.timer != null) {
+                holder.timer!!.cancel();
+            }
+
+            var timer: Long = numberList.get(position).toLong()
+            timer = timer*1000;
+            holder.timer = object : CountDownTimer(100000000, 1000) {
+                var number = holder.binding.textViewNumber.text.toString().toInt()
+                override fun onTick(millisUntilFinished: Long) {
+                    number++
+                    holder.binding.textViewNumber.setText(number.toString())
+                }
+
+                override fun onFinish() {
+                    holder.binding.textViewNumber.setText("0")
+                }
+            }.start()
+        }
+
     }
 
     override fun getItemCount(): Int {
