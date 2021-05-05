c833b29f15fdbe5e4b9b7bae7a087a64304a4f9b
abhinesh chandra <abhineshchandra1234@gmail.com>
2021-05-05 16:34:41 +0530

    added navigation file and matrix generation fragment

---
+++ app/src/main/java/com/example/matrixgeneration/MatrixGeneration.kt
@@ -0,0 +1,13 @@
+package com.example.matrixgeneration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MatrixGeneration : Fragment(R.layout.fragment_matrix_generation) {
+
+
+}
