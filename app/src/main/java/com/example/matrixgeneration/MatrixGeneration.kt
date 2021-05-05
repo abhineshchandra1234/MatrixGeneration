05d3df0130c10cee081ca2b39a486724f10895e3
abhinesh chandra <abhineshchandra1234@gmail.com>
2021-05-05 19:08:07 +0530

    added viewmodel and live data for number value

--- app/src/main/java/com/example/matrixgeneration/MatrixGeneration.kt
+++ app/src/main/java/com/example/matrixgeneration/MatrixGeneration.kt
@@ -1,13 +1,51 @@
 package com.example.matrixgeneration
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.matrixgeneration.databinding.FragmentMatrixGenerationBinding
 
 
 class MatrixGeneration : Fragment(R.layout.fragment_matrix_generation) {
+    private lateinit var binding: FragmentMatrixGenerationBinding
+    private lateinit var matrixGenerationViewModel: MatrixGenerationViewModel
+
+    companion object {
+        val TAG = "Main"
+    }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentMatrixGenerationBinding.bind(view)
+        super.onViewCreated(view, savedInstanceState)
+
+        //ViewModel
+        matrixGenerationViewModel = ViewModelProvider(this).get(MatrixGenerationViewModel::class.java)
+
+
+        viewClickListener()
+
+        matrixGenerationViewModel.getNumber().observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, "number value is "+it)
+        } )
+
+
+    }
+
+    private fun viewClickListener() {
+        binding.buttonSubmit.setOnClickListener {
+            if (binding.textInputEditTextNumber.text.toString().isEmpty()) {
+                Toast.makeText(context, "Please enter number", Toast.LENGTH_SHORT).show()
+            } else {
+                var number = binding.textInputEditTextNumber.text.toString().trim()
+                matrixGenerationViewModel.number.setValue(number.toInt())
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/matrixgeneration/MatrixGenerationViewModel.kt
+++ app/src/main/java/com/example/matrixgeneration/MatrixGenerationViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.matrixgeneration
 
-class MatrixGenerationViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class MatrixGenerationViewModel : ViewModel() {
+
+    val number = MutableLiveData<Int>()
+
+    fun getNumber(): LiveData<Int> {
+        return number
+    }
 }
